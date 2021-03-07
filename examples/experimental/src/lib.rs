bf4dbe09e8e6955bf5bb010d847d6e102c851c5b
Anders429 <anders.stefan.evensen@gmail.com>
2021-03-06 23:36:02 -0800

    Add experimental example.

---
+++ examples/experimental/src/lib.rs
@@ -0,0 +1,7 @@
+/// This is an experimental API.
+///
+/// This API is not guaranteed to be stable. It may change at any time.
+#[doc_item::docbox(content="<span class='emoji'>🔬</span> This is an experimental API.", class="unstable")]
+#[doc_item::short_docbox(content="Experimental", class="unstable")]
+#[doc_item::semi_transparent_item]
+pub fn foo() {}
