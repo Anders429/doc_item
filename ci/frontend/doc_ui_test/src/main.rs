452af1ff762f3fe2c7d8e20123cb99edf135fd11
Anders429 <anders.stefan.evensen@gmail.com>
2021-03-06 16:37:21 -0800

    Format URLs as files.

--- ci/frontend/doc_ui_test/src/main.rs
+++ ci/frontend/doc_ui_test/src/main.rs
@@ -52,58 +52,58 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .connect("http://localhost:4444")
         .await?;
         
-    let base_url = Path::new("file://").join(current_dir().unwrap().join(Path::new("../test_target/target/doc/test_target")));
+    let base_url = current_dir().unwrap().join(Path::new("../test_target/target/doc/test_target"));
     dbg!(&base_url);
     
     // Test individual doc pages.
-    client.goto(base_url.join("fn.function.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("fn.function.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<pre class=\"rust fn\">pub fn function()</pre>").await?;
     test_since_out_of_band(&mut client).await?;
     
-    client.goto(base_url.join("struct.Struct.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("struct.Struct.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<div class=\"docblock type-decl hidden-by-usual-hider\"><pre class=\"rust struct\">pub struct Struct {}</pre></div>").await?;
     test_since_out_of_band(&mut client).await?;
     
-    client.goto(base_url.join("enum.Enum.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("enum.Enum.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<div class=\"docblock type-decl\"><pre class=\"rust enum\">pub enum Enum {}</pre></div>").await?;
     test_since_out_of_band(&mut client).await?;
     
-    client.goto(base_url.join("constant.CONST.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("constant.CONST.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<pre class=\"rust const\">pub const CONST: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a> = 0;</pre>").await?;
     test_since_out_of_band(&mut client).await?;
     
-    client.goto(base_url.join("static.STATIC.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("static.STATIC.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<pre class=\"rust static\">pub static STATIC: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a></pre>").await?;
     test_since_out_of_band(&mut client).await?;
     
-    client.goto(base_url.join("union.Union.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("union.Union.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<div class=\"docblock type-decl hidden-by-usual-hider\"><pre class=\"rust union\">pub union Union {
     // some fields omitted
 }</pre></div>").await?;
     test_since_out_of_band(&mut client).await?;
 
-    client.goto(base_url.join("struct.Method.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("struct.Method.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<h4 id=\"method.method\" class=\"method\"><code>pub fn <a href=\"#method.method\" class=\"fnname\">method</a>()</code><span class=\"since\">1.0.0</span><a class=\"srclink\" href=\"../src/test_target/lib.rs.html#46\" title=\"goto source code\">[src]</a><a href=\"javascript:void(0)\" class=\"collapse-toggle\">[<span class=\"inner\">−</span>]</a></h4>").await?;
     test_since_standalone(&mut client, "<a class=\"srclink\" href=\"../src/test_target/lib.rs.html#46\" title=\"goto source code\">[src]</a>").await?;
     
-    client.goto(base_url.join("trait.Trait.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("trait.Trait.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<div class=\"docblock type-decl hidden-by-usual-hider\"><pre class=\"rust trait\">pub trait Trait { }</pre></div>").await?;
     test_since_out_of_band(&mut client).await?;
     
-    client.goto(base_url.join("struct.ImplTrait.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("struct.ImplTrait.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<h3 id=\"impl-Trait\" class=\"impl\"><code class=\"in-band\">impl <a class=\"trait\" href=\"../test_target/trait.Trait.html\" title=\"trait test_target::Trait\">Trait</a> for <a class=\"struct\" href=\"../test_target/struct.ImplTrait.html\" title=\"struct test_target::ImplTrait\">ImplTrait</a></code><a href=\"#impl-Trait\" class=\"anchor\"></a><span class=\"since\">1.0.0</span><a class=\"srclink\" href=\"../src/test_target/lib.rs.html#59\" title=\"goto source code\">[src]</a></h3>").await?;
     test_since_standalone(&mut client, "<a class=\"srclink\" href=\"../src/test_target/lib.rs.html#59\" title=\"goto source code\">[src]</a>").await?;
     
-    client.goto(base_url.join("module/index.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("module/index.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<h1 class=\"fqn\"><span class=\"out-of-band\"><span class=\"since\">1.0.0</span><span id=\"render-detail\"><a id=\"toggle-all-docs\" href=\"javascript:void(0)\" title=\"collapse all docs\">[<span class=\"inner\">−</span>]</a></span><a class=\"srclink\" href=\"../../src/test_target/lib.rs.html#65\" title=\"goto source code\">[src]</a></span><span class=\"in-band\">Module <a href=\"../index.html\">test_target</a>::<wbr><a class=\"mod\" href=\"\">module</a></span></h1>").await?;
     test_since_out_of_band(&mut client).await?;
     
-    client.goto(base_url.join("type.Type.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("type.Type.html").to_str().unwrap())).await?;
     test_docbox(&mut client, "<pre class=\"rust typedef\">type Type = <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>;</pre>").await?;
     test_since_out_of_band(&mut client).await?;
     
     // Test main doc page.
-    client.goto(base_url.join("index.html").to_str().unwrap()).await?;
+    client.goto(format!("file://{}", base_url.join("index.html").to_str().unwrap())).await?;
     test_short_docbox(&mut client, "function").await?;
     test_short_docbox(&mut client, "Struct").await?;
     test_short_docbox(&mut client, "Enum").await?;
