b6999a5c3768732e6ddce75fdf8ea492f437c33b
Anders429 <anders.stefan.evensen@gmail.com>
2021-04-06 02:45:06 -0700

    Update since attributes to use new argument syntax.

--- tests/test_target/src/lib.rs
+++ tests/test_target/src/lib.rs
@@ -4,37 +4,37 @@ use doc_item::{
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub fn function() {}
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub struct Struct {}
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub enum Enum {}
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub const CONST: usize = 0;
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub static STATIC: usize = 0;
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub union Union {
     pub a: usize,
@@ -44,30 +44,30 @@ pub struct Method {}
 
 impl Method {
     #[docbox(content = "docbox content", class = "docbox")]
-    #[since("1.0.0")]
+    #[since(content = "1.0.0")]
     pub fn method() {}
 }
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub trait Trait {}
 
 pub struct ImplTrait {}
 
 #[docbox(content = "docbox content", class = "docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 impl Trait for ImplTrait {}
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub mod module {}
 
 #[docbox(content = "docbox content", class = "docbox")]
 #[short_docbox(content = "short docbox content", class = "short-docbox")]
-#[since("1.0.0")]
+#[since(content = "1.0.0")]
 #[semi_transparent]
 pub type Type = usize;
