c4fa91276db1d3d5cf814903e03ecb80c707e92e
Anders429 <Anders429@users.noreply.github.com>
2021-04-09 01:08:07 +0000

    deploy: b58a420ff95ea80d1e308b990bf388e850f1aade

--- examples/search-index.js
+++ examples/search-index.js
@@ -1,6 +1,5 @@
 var searchIndex = JSON.parse('{\
 "custom_docbox":{"doc":"","t":[5],"n":["foo"],"q":["custom_docbox"],"d":[""],"i":[0],"f":[[[]]],"p":[]},\
-"doc_item":{"doc":"Attributes for item-level documentation customization.","t":[23,23,23,23],"n":["docbox","semi_transparent","short_docbox","since"],"q":["doc_item","","",""],"d":["Adds a docbox to the item’s item-info.","Makes an item semi-transparent in module lists.","Adds a short docbox to the item in module lists.","Adds a minimal version to an item."],"i":[0,0,0,0],"f":[null,null,null,null],"p":[]},\
 "experimental":{"doc":"","t":[5],"n":["foo"],"q":["experimental"],"d":["The docbox will indicate the function is experimental. It …"],"i":[0],"f":[[[]]],"p":[]}\
 }');
 initSearch(searchIndex);
--- examples/source-files.js
+++ examples/source-files.js
@@ -1,5 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["custom_docbox"] = {"name":"","files":["custom_docbox.rs"]};
-sourcesIndex["doc_item"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["experimental"] = {"name":"","files":["experimental.rs"]};
 createSourceSidebar();
