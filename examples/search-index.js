f4a4cebb32e1aa07620d4a836f96a29f89da7ca7
Anders429 <Anders429@users.noreply.github.com>
2021-04-18 01:13:19 +0000

    deploy: a0c1b40ffe0ed73566b4148d0f5426d44508675d

--- examples/search-index.js
+++ examples/search-index.js
@@ -2,4 +2,4 @@ var searchIndex = JSON.parse('{\
 "custom_docbox":{"doc":"","t":[5],"n":["foo"],"q":["custom_docbox"],"d":[""],"i":[0],"f":[[[]]],"p":[]},\
 "experimental":{"doc":"","t":[5],"n":["foo"],"q":["experimental"],"d":["The docbox will indicate the function is experimental. It …"],"i":[0],"f":[[[]]],"p":[]}\
 }');
-initSearch(searchIndex);
+if (window.initSearch) {window.initSearch(searchIndex)};
