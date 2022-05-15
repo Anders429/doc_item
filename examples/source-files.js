51b6de78dcfc1296df58f39f794347a3621c7283
Anders429 <Anders429@users.noreply.github.com>
2022-05-15 01:06:49 +0000

    deploy: 009c7a89fab703c8173199bdd76f0411bc84ad80

--- examples/source-files.js
+++ examples/source-files.js
@@ -1,4 +1,4 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["custom_docbox"] = {"name":"","files":["custom_docbox.rs"]};
 sourcesIndex["doc_item"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["experimental"] = {"name":"","files":["experimental.rs"]};
