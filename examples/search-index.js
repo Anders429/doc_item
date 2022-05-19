ebc9c42f9eb4c07180883d6023399c1b0969d6c0
Anders429 <Anders429@users.noreply.github.com>
2022-05-19 01:02:30 +0000

    deploy: 009c7a89fab703c8173199bdd76f0411bc84ad80

--- examples/search-index.js
+++ examples/search-index.js
@@ -3,4 +3,5 @@ var searchIndex = JSON.parse('{\
 "doc_item":{"doc":"Attributes for item-level documentation customization.","t":[23,23,23,23],"n":["docbox","semi_transparent","short_docbox","since"],"q":["doc_item","","",""],"d":["Adds a docbox to the item’s item-info.","Makes an item semi-transparent in module lists.","Adds a short docbox to the item in module lists.","Adds a minimal version to an item."],"i":[0,0,0,0],"f":[null,null,null,null],"p":[]},\
 "experimental":{"doc":"","t":[5],"n":["foo"],"q":["experimental"],"d":["The docbox will indicate the function is experimental. It …"],"i":[0],"f":[[[]]],"p":[]}\
 }');
-if (window.initSearch) {window.initSearch(searchIndex)};
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
