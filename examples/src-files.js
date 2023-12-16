92d8e13f3279efebca02bd18f58bd663e735a33f
Anders429 <Anders429@users.noreply.github.com>
2023-12-16 00:41:43 +0000

    deploy: 009c7a89fab703c8173199bdd76f0411bc84ad80

--- examples/src-files.js
+++ examples/src-files.js
@@ -1,6 +1,6 @@
-var srcIndex = JSON.parse('{\
-"custom_docbox":["",[],["custom_docbox.rs"]],\
-"doc_item":["",[],["lib.rs"]],\
-"experimental":["",[],["experimental.rs"]]\
-}');
+const srcIndex = new Map(JSON.parse('[\
+["custom_docbox",["",[],["custom_docbox.rs"]]],\
+["doc_item",["",[],["lib.rs"]]],\
+["experimental",["",[],["experimental.rs"]]]\
+]'));
 createSrcSidebar();
