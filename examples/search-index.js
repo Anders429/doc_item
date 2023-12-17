fe5470b720e57b64ab77c28a394999fe760794d9
Anders429 <Anders429@users.noreply.github.com>
2023-12-17 00:45:49 +0000

    deploy: 009c7a89fab703c8173199bdd76f0411bc84ad80

--- examples/search-index.js
+++ examples/search-index.js
@@ -1,7 +1,7 @@
-const searchIndex = new Map(JSON.parse('[\
+var searchIndex = new Map(JSON.parse('[\
 ["custom_docbox",{"doc":"","t":"H","n":["foo"],"q":[[0,"custom_docbox"]],"d":[""],"i":[0],"f":[[[],1]],"c":[],"p":[[1,"tuple"]],"b":[]}],\
 ["doc_item",{"doc":"Attributes for item-level documentation customization.","t":"XXXX","n":["docbox","semi_transparent","short_docbox","since"],"q":[[0,"doc_item"]],"d":["Adds a docbox to the item’s item-info.","Makes an item semi-transparent in module lists.","Adds a short docbox to the item in module lists.","Adds a minimal version to an item."],"i":[0,0,0,0],"f":[0,0,0,0],"c":[],"p":[],"b":[]}],\
 ["experimental",{"doc":"","t":"H","n":["foo"],"q":[[0,"experimental"]],"d":["The docbox will indicate the function is experimental. It …"],"i":[0],"f":[[[],1]],"c":[],"p":[[1,"tuple"]],"b":[]}]\
 ]'));
-if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
-if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
--- examples/src-files.js
+++ examples/src-files.js
@@ -1,4 +1,4 @@
-const srcIndex = new Map(JSON.parse('[\
+var srcIndex = new Map(JSON.parse('[\
 ["custom_docbox",["",[],["custom_docbox.rs"]]],\
 ["doc_item",["",[],["lib.rs"]]],\
 ["experimental",["",[],["experimental.rs"]]]\
