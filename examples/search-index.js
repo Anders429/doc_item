195d41e527778f33c59dbc7d1901b6acdb435d89
Anders429 <Anders429@users.noreply.github.com>
2024-04-20 00:41:42 +0000

    deploy: 009c7a89fab703c8173199bdd76f0411bc84ad80

--- examples/search-index.js
+++ examples/search-index.js
@@ -1,7 +1,7 @@
 var searchIndex = new Map(JSON.parse('[\
-["custom_docbox",{"t":"H","n":["foo"],"q":[[0,"custom_docbox"]],"i":[0],"f":"{{}b}","D":"`","p":[[1,"unit"]],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA="}],\
-["doc_item",{"t":"XXXX","n":["docbox","semi_transparent","short_docbox","since"],"q":[[0,"doc_item"]],"i":[0,0,0,0],"f":"````","D":"j","p":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}],\
-["experimental",{"t":"H","n":["foo"],"q":[[0,"experimental"]],"i":[0],"f":"{{}b}","D":"b","p":[[1,"unit"]],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAAAEAAAAAAA"}]\
+["custom_docbox",{"t":"H","n":["foo"],"q":[[0,"custom_docbox"]],"i":[0],"f":"{{}b}","D":"`","p":[[1,"unit"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA="}],\
+["doc_item",{"t":"XXXX","n":["docbox","semi_transparent","short_docbox","since"],"q":[[0,"doc_item"]],"i":[0,0,0,0],"f":"````","D":"j","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}],\
+["experimental",{"t":"H","n":["foo"],"q":[[0,"experimental"]],"i":[0],"f":"{{}b}","D":"b","p":[[1,"unit"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAAAEAAAAAAA"}]\
 ]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
