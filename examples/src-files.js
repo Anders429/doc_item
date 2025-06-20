537a4b1c35a0ee31a88d694a49eca4ecd65d733b
Anders429 <Anders429@users.noreply.github.com>
2025-06-20 01:05:18 +0000

    deploy: 009c7a89fab703c8173199bdd76f0411bc84ad80

--- examples/src-files.js
+++ examples/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["custom_docbox",["",[],["custom_docbox.rs"]]],["doc_item",["",[],["lib.rs"]]],["experimental",["",[],["experimental.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[46,32,45]}
+createSrcSidebar('[["custom_docbox",["",[],["custom_docbox.rs"]]],["doc_item",["",[],["lib.rs"]]],["experimental",["",[],["experimental.rs"]]]]');
+//{"start":19,"fragment_lengths":[46,32,45]}
